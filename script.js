694268cd356db215e269c06d8ae85b70a75c0984
cabbagestamps <cabbagestamps@gmail.com>
2022-03-08 20:11:11 +1100

    add DOM elements and interaction with buttons add scorecard, removed obsolete functions, removed alerts, add semi colons

--- script.js
+++ script.js
@@ -1,72 +1,82 @@
 function computerPlay() {
 let randomInt = Math.floor(Math.random() * 3);  
 if (randomInt === 0) {
+    compChoiceText ='rock';
     return 'Rock';
   } else if (randomInt === 1) {
-    return 'Paper';
+    compChoiceText = 'paper';
+    return  'Paper';
   } else {
-    return 'Scissors';
+    compChoiceText = 'scissors';
+    return  'Scissors';
   }
 };
 
-function game() {
-  let wins = 0;
-  let draws = 0;
-  let losses = 0;
-  let roundsLeft = 5
-  while (roundsLeft > 0) {
-    playRound() 
-    --roundsLeft;
-  };
+let wins = 0;
+let draws = 0;
+let losses = 0;
+let roundsPlayed = 0
+//  while (roundsLeft > 0) {
+ //   playRound() 
+ //   --roundsLeft;
+ // }
 
 
- function playRound(playerSelection, computerSelection) { 
-    playerSelection = prompt(`${roundsLeft} rounds remaining. Choose your weapon:`, '');
+function playRound(playerSelection, computerSelection) { 
     computerSelection = computerPlay();
-     if (playerSelection === null) {
-        alert('Game cancelled')  
-        roundsLeft = 0;
-        return;
-      } else {
-        if ((playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'scissors') ||
-              (playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'rock') || 
-              (playerSelection.toLowerCase() === 'scissors' && computerSelection.toLowerCase() === 'paper')) {
-        alert(`You selected: ${playerSelection}. The computer plays ${computerSelection}. You win!`);
-        return ++wins;
-      } else if (playerSelection.toLowerCase() === computerSelection.toLowerCase())  {
-        alert(`You selected: ${playerSelection}. The computer plays ${computerSelection}. It's a draw!`);
-        return ++ draws;
-      } else if (playerSelection === '') {
-        alert(`You didn't select anything! The computer plays ${computerSelection}. You lose!`)
-        return ++losses;
-      } else if (!isValidEntry) {
-         alert(`${playerSelection} is not a valid weapon. Please choose another.`)
-         roundsLeft = roundsLeft + 1;
-         return;
-      } else {
-        alert(`You selected: ${playerSelection}. The computer plays ${computerSelection}. You lose!`); 
-        return ++losses;
-      };  
-    };
-  };
-  console.log(`Wins =  ${wins}`);
-  console.log(`Draws = ${draws}`);
-  console.log(`Losses = ${losses}`);
-  if (wins > losses) {
-      return alert('A grand victory');
-  } else if (wins < losses) {
-      return alert('A miserable defeat');
-  } else  {
-      return alert('A confusing draw');
-  };
-};  
-
-function isValidEntry() {
-  if  (playerSelection.toLowerCase() === 'rock' ||
-        playerSelection.toLowerCase() === 'paper' ||
-        playerSelection.toLowerCase() === 'scissors') {
-      return true;
+  if ((playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'scissors') ||
+          (playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'rock') || 
+          (playerSelection.toLowerCase() === 'scissors' && computerSelection.toLowerCase() === 'paper')) {
+    roundResult = 'Win';
+    ++roundsPlayed;
+    return ++wins;
+  } else if (playerSelection.toLowerCase() === computerSelection.toLowerCase())  {
+      roundResult = 'Draw';
+      ++roundsPlayed;
+      return ++ draws;
   } else {
-    return false;
-  }
+      roundResult = 'Loss';
+      ++roundsPlayed;
+      return ++losses;
+  };  
 };
+
+let roundResult;
+let compChoiceText;
+
+function updateResults(){
+  document.getElementById('numRounds').textContent = roundsPlayed;
+  document.getElementById('wins').textContent = wins;
+  document.getElementById('draws').textContent = draws;
+  document.getElementById('losses').textContent = losses;
+};
+
+let result = document.getElementById('result');
+let compSelDiv = document.getElementById('compSelDiv') ;
+
+let rockButton = document.querySelector('#rock');
+rockButton.addEventListener('click', () => {
+  console.log('rock');
+  playRound('rock');
+  compSelDiv.textContent = 'The Computer chose: ' + compChoiceText;
+  result.textContent = 'This round resulted in a: ' + roundResult;
+  updateResults();
+});
+
+let paperButton = document.querySelector('#paper');
+paperButton.addEventListener('click', () => {
+  console.log('paper');
+  playRound('paper');
+  compSelDiv.textContent = 'The Computer chose: ' + compChoiceText;
+  result.textContent = 'This round resulted in a: ' + roundResult;
+  updateResults();
+});
+
+let scissorsButton = document.querySelector('#scissors');
+scissorsButton.addEventListener('click', () => {
+  console.log('scissors');
+  playRound('scissors');
+  compSelDiv.textContent = 'The Computer chose: ' + compChoiceText;
+  result.textContent = 'This round resulted in a: ' + roundResult;
+  updateResults();
+});
